ee299ed027cc1450f142d38a5fa46429a10bd4b8
gnnort <71447950+gnnort@users.noreply.github.com>
2021-11-27 17:22:03 +0800

    Added 'click' module commands and outline of code functions
    Summary:
    -Added commands from python 'click' module for writing CLI scripts.
    -Added planned functionality for the ticket viewer i.e "view_all_tickets" and "ticket_detail"

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import click
 import requests
 
+@click.group()
+def cli():
+    "To be modified"
+
+@cli.command()
+def view_all_tickets():
+    pass
+
+@cli.command()
+def ticket_detail():
+    pass
+
+if __name__ == '__main__':
+    cli()
