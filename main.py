0cb4d5a8a12650b19252dc0eee5c6595fbc00916
gnnort <71447950+gnnort@users.noreply.github.com>
2021-11-28 11:20:35 +0800

    Added code for 'view-all-tickets' function
    Summary:
    - Added python code to retrieve tickets from zendesk API using 'requests' library
    - Broke up page size to chunks of 25 tickets as per challenge requirements
    - Using cursor pagination to retrieve next page of tickets
    -

--- main.py
+++ main.py
@@ -7,7 +7,28 @@ def cli():
 
 @cli.command()
 def view_all_tickets():
-    pass
+    url = "https://tron7825.zendesk.com/api/v2/tickets" + ".json" + "?page[size]=25"   #each page will have 25 tickets #what is the .json for?
+    user = "ngtron25@gmail.com/token"
+    pw = "IN5Kog01geHQSmjZ2cUN2N9dDCFEqBM1py59zrlW"
+    page_count = 0
+    while url:
+        page_count += 1
+        response = requests.get(url, auth= (user,pw))
+        data = response.json() #decode to python dict
+        if response.status_code > 299:
+            print("Error", response.status_code)
+            quit
+        else:
+            ticketList = []
+            for ticket in data['tickets']: #data is a dictionary with 'tickets' as a key and a list of tickets as the value
+                ticketList.append("Subject: " + "-" + ticket['subject'] + "-" + " Ticket ID is " + str(ticket['id'])) #convert to fstring?
+            print(f"Page: {page_count}")
+            print('\n'.join(ticketList)) #every elem in list is joined by a new line
+            print('\n')
+            if data['meta']['has_more']:  #cursor pagination
+                url = data['links']['next']
+            else:
+                url = None
 
 @cli.command()
 def ticket_detail():
