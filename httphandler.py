c9a334c22ef7dca5221c68b65cfaa52811fabd53
gnnort <71447950+gnnort@users.noreply.github.com>
2021-12-01 23:12:37 +0800

    Added simple html on redirect page to inform user that window can be closed
    - also added cute emoji!

--- httphandler.py
+++ httphandler.py
@@ -15,10 +15,17 @@ class Requesthandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-Type:', 'text/html')              #details content type that page will display
         self.end_headers()
+        htmlList = [
+        '<html><body>',
+        '<h2><center>Authorized! You may now close this window &#9786;</center><h2>'
+                    ]
+        output = ''
+        for line in htmlList:
+            output += line
         if 'code' in self.path:
             global CODE 
             CODE = self.path[7:]                                    #extract code from link
-            
+            self.wfile.write(output.encode())
 
 class Httpserver(HTTPServer):
     timeout = 10                                                    #seconds until handle request times out
